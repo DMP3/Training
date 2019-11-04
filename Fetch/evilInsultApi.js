6cf08422ed2a7c1118499a181db37d2751592055
DMP3 <dimomilenov@gmail.com>
2019-11-04 17:34:08 +0200

    evilInsult init

---
+++ Fetch/evilInsultApi.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const URL = 'https://evilinsult.com/generate_insult.php?lang=en&type=json';
+
+const getEvilInsult = async() => {
+    let response = await fetch(URL);
+    let data = await response.json();
+
+    return data.insult;
+}
+
+const createStaticInsultPage = async() => {
+    let insultText = await getEvilInsult();
+    console.log(insultText);
+    let html = `
+    <body style="display:flex; flex-direction:column; justify-content:center; min-height:100vh;">
+        <h1 style="margin: 0 auto; background: rgb(235, 235, 235); text-align: center;">${insultText}</h1>
+    </body>`;
+    fs.writeFile("table.html", html, (err) => {
+        if (err) throw err;
+        console.log('File saved!');
+    });
+}
+
+function logEvery10Seconds(i) {
+    setTimeout(() => {
+        createStaticInsultPage();
+        logEvery10Seconds(++i);
+    }, 10000)
+}
+
+logEvery10Seconds(0);
