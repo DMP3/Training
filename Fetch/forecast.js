977b6fd8ee5f5d4ab227dba8888f46da25c73759
DMP3 <dimomilenov@gmail.com>
2019-11-04 12:08:56 +0200

    removed writing to file

--- Fetch/forecast.js
+++ Fetch/forecast.js
@@ -1,20 +1,44 @@
 const fetch = require('node-fetch');
-const fs = require('fs');
-
-const outputFile = 'fuckinVarnaWeather.json';
 
 const APIKEY = '7b1a4064ff079e43861773552d747ffd';
-let cityId = 726050;
-let queryUnits = 'metric';
+let cityId = 726050; //VarnaID
+let units = 'metric';
 
-const forecastURL = `http://api.openweathermap.org/data/2.5/weather?id=${cityId}&APPID=${APIKEY}&units=${queryUnits}`;
+const forecastURL = `http://api.openweathermap.org/data/2.5/weather?id=${cityId}&APPID=${APIKEY}&units=${units}`;
 
-let getData = async (url) => {
-    fetch(url)
-    .then(res => res.json())
-    .then(json => {
-        fs.writeFile(outputFile, JSON.stringify(json), (err) => { if(err) throw err; })
-    })
+let getDataToFile = async(url) => {
+    let response = await fetch(url);
+    let data = await response.json();
+
+    let {
+        name: cityName
+    } = data;
+
+    let {
+        temp: temperature,
+        temp_min,
+        temp_max,
+    } = data.main;
+
+    let {
+        speed: windSpeed
+    } = data.wind;
+
+    let {
+        main: weatherInfo,
+        description: weatherDescriprion
+    } = data.weather[0];
+
+    console.log(`
+        ---${cityName} weather---\n
+        Temp: ${temperature}°C\n
+        Min/Max temp: ${temp_min}°C/${temp_max}°C\n
+        WindSpeed: ${windSpeed} km/h\n
+        Weather info: ${weatherInfo} / ${weatherDescriprion}\n
+        `);
+
+    var ts = new Date();
+    console.log(`Last update: ${ts.toLocaleString()}`);
 }
 
-getData(forecastURL);
+let forecastData = getDataToFile(forecastURL);
