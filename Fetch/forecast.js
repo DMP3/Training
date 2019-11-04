beb3bdf19dacd83f87081a6ec2af44a29a3b8b49
DMP3 <dimomilenov@gmail.com>
2019-11-04 12:32:35 +0200

    addeed comments

--- Fetch/forecast.js
+++ Fetch/forecast.js
@@ -11,29 +11,29 @@ let showForecast = async(url) => {
     let data = await response.json();
 
     let {
-        name: cityName
+        name: cityName // cityName = data.name
     } = data;
 
     let {
-        temp: temperature,
+        temp: temperature, // temperature = data.main.temp
         temp_min,
         temp_max,
     } = data.main;
 
     let {
-        speed: windSpeed
+        speed: windSpeed // windSpeed = data.wind.speed
     } = data.wind;
 
     let {
-        main: weatherInfo,
-        description: weatherDescriprion
+        main: weatherInfo, // weatherInfo = data.weather[0].main
+        description: weatherDescriprion // weatherDescriprion = data.weather[0].description
     } = data.weather[0];
 
     console.log(`
         ---${cityName} weather---\n
         Temp: ${temperature}°C\n
         Min/Max temp: ${temp_min}°C/${temp_max}°C\n
-        WindSpeed: ${windSpeed} km/h\n
+        Wind Speed: ${windSpeed} km/h\n
         Weather info: ${weatherInfo} / ${weatherDescriprion}\n
         `);
 
