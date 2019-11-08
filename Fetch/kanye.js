2873a94f0127bf4d5002e554c3aa59fe9965843e
DMP3 <dimomilenov@gmail.com>
2019-11-08 17:20:49 +0200

    json kanye with stat code validation

--- Fetch/kanye.js
+++ Fetch/kanye.js
@@ -1,11 +1,18 @@
-const URL = 'https://api.kanye.rest?format=text';
+const URL = 'https://api.kanye.rest?format=json';
 const fetch = require('node-fetch');
 const fs = require('fs');
 
-const getKanye = async() => {
-    const response = await fetch(URL);
-    const data = await response.text();
-    console.log(data);
-}
+const getKanye = async () => {
+  const res = await fetch(URL);
+  let data = null;
+  if (res.ok) { // res.status >= 200 && res.status < 300
+    data = await res.json();
+    data = data.quote;
+  } else {
+    data = res.statusText;
+    //throw MyCustomError(res.statusText);
+  }
+  console.log(data);
+};
 
 getKanye();
